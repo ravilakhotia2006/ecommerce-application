18085463ec4e309b9086382fb11dab72d87c810c
ravilakhotia <ravilakhotia2006@gmail.com>
2019-09-10 08:30:54 +0530

    first commit

---
+++ starter_code/src/main/java/com/example/demo/security/AuthProperties.java
@@ -0,0 +1,25 @@
+package com.example.demo.security;
+
+
+public class AuthProperties {
+
+	/**
+	 * Instantiates a new auth properties.
+	 */
+	private AuthProperties() {
+
+	}
+
+	/** The Constant SECRET. */
+	public static final String SECRET = "GenerateToken";
+	
+	/** The Constant EXPIRATION_TIME. */
+	public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+	
+	/** The Constant TOKEN_PREFIX. */
+	public static final String TOKEN_PREFIX = "Bearer ";
+	
+	/** The Constant HEADER_STRING. */
+	public static final String HEADER_STRING = "Authorization";
+
+}
